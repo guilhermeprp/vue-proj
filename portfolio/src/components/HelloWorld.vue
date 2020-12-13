f66b6b647e8af78338eab77658472592cfc4d30b
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2020-12-13 20:36:26 -0300

    first commit

---
+++ portfolio/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+<nav>
+
+  <div class="header">
+  <div class="nav-bar">                 
+  
+  </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
