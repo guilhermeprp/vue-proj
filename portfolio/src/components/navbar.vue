56853c4b05711dafb6c0e0e7f3abd3ca9b28b7e1
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2020-12-23 07:03:33 -0300

    att

--- portfolio/src/components/navbar.vue
+++ portfolio/src/components/navbar.vue
@@ -9,7 +9,15 @@
             <span class="nav-logo-subtitle"> Front end Dev.</span>
           </label>
         </div>
-        <div class="menu-toggle" />
+        <div class="box-menu-button">
+          <div class="menu-icon-box">
+            <div class="menu-icon-bar" />
+            <div class="menu-icon-bar-off" />
+            <div id="mid-bar" class="menu-icon-bar" />
+            <div class="menu-icon-bar-off" />
+            <div class="menu-icon-bar" />
+          </div>
+        </div>
       </div>
     </div>
   </nav>
