82b042cca6ee21a9fa373b0a38fc117b69a2a1d3
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-01-19 18:46:14 -0300

    img logos added to cards

--- portfolio/src/components/skills-section.vue
+++ portfolio/src/components/skills-section.vue
@@ -2,30 +2,50 @@
   <section class="skills-section">
     <div class="wrapper">
       <h1>Skills</h1>
-      <h5>Pass the mouse over the boxes and discover my skills:</h5>
+      <h5>Pass the mouse over the cards and discover my skills:</h5>
       <div class="container">
         <div class="card">
           <div class="content">
-            PRIMEIRO
-            <br />
-            Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
-            iuashdi
+            <div>
+              <img src="../assets/images/logo-react.png" />
+              <h2>
+                REACT
+              </h2>
+              <br />
+              <p>
+                Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
+                iuashdi
+              </p>
+            </div>
           </div>
         </div>
         <div class="card">
           <div class="content">
-            SEGUNDO
-            <br />
-            Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
-            iuashdi
+            <div>
+              <img src="../assets/images/logo-vuejs.png" />
+              <h2>
+                VUE JS
+              </h2>
+              <br />
+              <p>
+                Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
+                iuashdi
+              </p>
+            </div>
           </div>
         </div>
         <div class="card">
           <div class="content">
-            TERCEIRO
-            <br />
-            Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
-            iuashdi
+            <div>
+              <h2>
+                TERCEIRO
+              </h2>
+              <br />
+              <p>
+                Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
+                iuashdi
+              </p>
+            </div>
           </div>
         </div>
       </div>
