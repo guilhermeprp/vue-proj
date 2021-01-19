552b77ed00b57794a2f6384933ae6e9adec11c55
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-01-19 05:07:23 -0300

    skill section html created

---
+++ portfolio/src/components/skills-section.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="skills-section">
+    <div class="wrapper">
+      <h1>Skills</h1>
+      <h5>Pass the mouse over the boxes and discover my skills:</h5>
+      <div class="container">
+        <div class="card">
+          <div class="content">
+            PRIMEIRO
+            <br />
+            Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
+            iuashdi
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            SEGUNDO
+            <br />
+            Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
+            iuashdi
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            TERCEIRO
+            <br />
+            Ahusahudh iasodhoash hjashijdha ii asiuhas dhsai diuas huiasdh
+            iuashdi
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
